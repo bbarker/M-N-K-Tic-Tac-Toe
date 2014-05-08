2079d8bc6951d3fb3e52ce9afd7816321f4ce22c
Brandon Barker <brandon.barker@gmail.com>
2014-05-08 14:44:22 -0400

    working printing function

--- c-protype/printBoard.c
+++ c-protype/printBoard.c
@@ -3,29 +3,32 @@
 
 int main(void) {
 
+typedef unsigned int board_idx;
 // To be input variables:
 // m (cols)
-unsigned int m = 3;
+board_idx m = 3;
 // n (rows)
-unsigned int n = 3;
+board_idx n = 3;
 // board state : m x n char array
 
-char board_state[m*n] = 
+char board_state[] = 
   {'x', ' ', 'x',
    'o', 'o', ' ',
    ' ', ' ', ' '
   }; 
 // cursor position
 
-printf("Testing.\n\n");
-
 //0, 0 corresponds to upper left corner
-for (uint i = 0; i < n; i++) {
+board_idx i;
+board_idx j;
+for (i = 0; i < n; i++) {
   putc(' ', stdout); 
-  for (uint j = 0; j < m; j++) {
+  for (j = 0; j < m; j++) {
     putc('|', stdout);
     putc(board_state[i*m + j], stdout);
   }
   putc('|', stdout);
   putc('\n', stdout);
 }
+
+} // end of [main]
