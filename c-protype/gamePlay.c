a10159d7a6f4acac3578d0416a9dc1f6c7484878
bbarker <brandon.barker@gmail.com>
2014-05-12 02:14:40 -0400

    did a bit more refactoring

--- c-protype/gamePlay.c
+++ c-protype/gamePlay.c
@@ -7,32 +7,30 @@
 /* ** ** local function declarations ** ** */
 int countInDirection(board_conf_type board_conf, board_type board, 
   direction scoring_direction);
+bool isInBounds(board_conf_type board_conf, int x, int y);
 /* ** end local function declarations ** */
+
+
 
 
 bool isWinner(board_conf_type board_conf, board_type board) {
 unsigned short lineSums[NUM_SCORE_LINES] = {1, 1, 1, 1}; 
-
-// Need to check both diagonals and both cardinal axes
-
-direction scoring_direction;
-
-{int line;
 //Iterate over all directions:
+int line;
+direction scoring_direction;
 for (int i = 0; i < 8; i++) {
-  line = i % 4;
+  line = i % NUM_SCORE_LINES;
   scoring_direction.dx = (int)      nearbyint(cos(i*PI / 4.0));
   scoring_direction.dy = (int) -1 * nearbyint(sin(i*PI / 4.0));
   lineSums[line] += countInDirection(board_conf, board, scoring_direction);
-}}
-
-
+}
+//
 unsigned short lineSumMax = 1;
 for (unsigned short i = 0; i < NUM_SCORE_LINES; ++i) {
   if (lineSums[i] > lineSumMax)
     { lineSumMax = lineSums[i]; }
 } // end of [for]
-
+//
 if (lineSumMax >= board_conf.k) {
   return true;
 } else {
@@ -45,30 +43,37 @@ if (lineSumMax >= board_conf.k) {
   Helper function for [checkWinner]. 
   Counts contiguous elements of the same type as origin in
   specified direction (steps) and returns the sum of these.
-
 */
 int countInDirection(board_conf_type board_conf, board_type board, 
   direction scoring_direction) {
+//
+const int dx = scoring_direction.dx;
+const int dy = scoring_direction.dy;
 //
 board_element elementAtCursor = 
   board[board_conf.cursor.y * board_conf.m + board_conf.cursor.x];
 board_element xPos = board_conf.cursor.x, yPos = board_conf.cursor.y;
 unsigned short lineSum = 0;
-while (xPos + scoring_direction.dx < board_conf.m && 
-       yPos + scoring_direction.dy < board_conf.n && 
-       xPos + scoring_direction.dx >= 0 && 
-       yPos + scoring_direction.dy >= 0) {
-xPos = xPos + scoring_direction.dx;
-yPos = yPos + scoring_direction.dy;
-if (elementAtCursor == board[yPos * board_conf.m + xPos]) {
-  ++lineSum;
-} else {
-  break;
-} // end of [if (elementAtCursor //]
+while (isInBounds(board_conf, xPos + dx, yPos + dy)) {
+  xPos = xPos + dx;
+  yPos = yPos + dy;
+  if (elementAtCursor == board[yPos * board_conf.m + xPos]) {
+    ++lineSum;
+  } else {
+    break;
+  } // end of [if (elementAtCursor //]
 } // end of [while]
 
 return lineSum;
 } // end of [countInDirection]
 
 
-// bool isIncrementInBounds(board_con
+bool isInBounds(board_conf_type board_conf, int x, int y) {
+if (x < board_conf.m && y < board_conf.n && 
+    x >= 0           && y >= 0)
+{
+  return true;
+} else {
+  return false;
+} // end of [if]
+} // end of [isInBounds]
